eb7fcf01085aec7eff3c170f7cd6cb6a34c5a0b1
benittoraj <brendonbeni42@gmail.com>
2020-08-08 16:02:34 +0530

    class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import image from './kane.jpg'
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {isShowing:false};
+    // this.handleClick = this.handleClick.bind(this);
+    console.log('constructor executed')
+  }
+  handleClick = () => {   // NO need to bind if we use arrow function
+         this.setState({isShowing:!this.state.isShowing})
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // handleClick() {  // need to bind this keyword to this function in constructor
+  //   this.setState({isShowing:!this.state.isShowing})
+  // }
+
+  componentDidMount(){
+     console.log('app mounted')
+  }
+
+  componentWillUnMount(){
+    
+  }
+    render() {
+      console.log('app rendered')
+        return (
+         <div className = "App container" >
+            <section className="d-flex justify-content-center">
+              <button className = "p-1 text-primary " onClick={this.handleClick} > toggle image < /button>
+            </section>
+
+            <section className = " py-2 d-flex justify-content-center" >
+               {
+                this.state.isShowing ? (<img src={image} alt="missing" />):("")
+               }
+            </section> 
+          < /div>);
+    }
 }
-
 export default App;
